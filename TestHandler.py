aecd3da569e64474cf49faddd2b45c69530a90d9
dinukapryzm <dinuka.arseculeratne@pryzm.health>
2020-11-04 18:09:07 +1100

    made tests take the tag values directly from the environment variables in the serverless section

--- TestHandler.py
+++ TestHandler.py
@@ -1,6 +1,7 @@
 import os
 
 import boto3
+import yaml
 from moto import mock_ec2
 
 import handler as handler
@@ -9,13 +10,15 @@ import handler as handler
 @mock_ec2
 def test_ec2_stop():
     # handler.ec2_stop(None,None)
-    os.environ['TAGS'] = 'Auto=On'
+    serverless_yaml = readYaml()
+    tags_from_serverless_yaml = serverless_yaml['provider']['environment']['TAGS']
+    os.environ['TAGS'] = tags_from_serverless_yaml
     region = 'us-west-1'
     client = boto3.client('ec2', region_name=region)
     reservation = client.run_instances(ImageId='ami-1234abcd', MinCount=1, MaxCount=1)
     instance_id = reservation['Instances'][0]['InstanceId']
 
-    tags = [{"Key": "Auto", "Value": "On"}]
+    tags = list(map(lambda x: create_tag_obj(x), tags_from_serverless_yaml.split(",")))
 
     client.create_tags(Resources=[instance_id], Tags=tags)
 
@@ -27,6 +30,23 @@ def test_ec2_stop():
     instance_id_list = list(map(lambda x: x.id, instances))
     filtered_instance_ids = list(filter(lambda x: x == instance_id, instance_id_list))
     assert instance_id in filtered_instance_ids
+
+
+def create_tag_obj(tag):
+    tag_split = tag.split("=")
+    tag_key = tag_split[0]
+    tag_value = tag_split[1]
+    return {'Key': tag_key,
+            'Value': tag_value}
+
+
+def readYaml():
+    with open("serverless.yml", "r") as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exec:
+            print(exec)
+            sys.exit(1)
 
 
 if __name__ == '__main__':
