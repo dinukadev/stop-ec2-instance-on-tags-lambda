498fcbc715794a3e1eec63ca1591154596a73437
dinukapryzm <dinuka.arseculeratne@pryzm.health>
2020-11-09 13:40:32 +1100

    initial commit for metricon related tagging

--- TestHandler.py
+++ TestHandler.py
@@ -1,6 +1,8 @@
 import os
+from datetime import datetime, timedelta
 
 import boto3
+import pytz
 import yaml
 from moto import mock_ec2
 
@@ -8,16 +10,27 @@ import handler as handler
 
 
 @mock_ec2
-def test_should_stop_ec2_instances_with_matching_tags():
+def should_stop_ec2_instances_for_24x5_Mon_Fri_tag():
     serverless_yaml = readYaml()
-    tags_from_serverless_yaml = serverless_yaml['provider']['environment']['TAGS']
-    os.environ['TAGS'] = tags_from_serverless_yaml
-    region = 'us-west-1'
+    # tags_from_serverless_yaml = serverless_yaml['provider']['environment']['AVAILABILITY_TAG_VALUES']
+    os.environ['AVAILABILITY_TAG_VALUES'] = '24x5_Mon-Fri'
+    now = datetime.now()
+    now = now.replace(hour=0, second=0, microsecond=0, minute=1)
+    local_tz = pytz.timezone('Australia/Sydney')
+    local_time = now.astimezone(local_tz)
+    day_int = now.weekday()
+    if day_int <= 5:
+        local_time = local_time + timedelta(days=5 - day_int)
+    else:
+        local_time = local_time + timedelta(days=day_int + 6)
+    os.environ['CURR_TIME'] = local_time.strftime("%m/%d/%Y, %H:%M:%S")
+
+    region = 'ap-southeast-2'
     client = boto3.client('ec2', region_name=region)
     reservation = client.run_instances(ImageId='ami-1234abcd', MinCount=1, MaxCount=1)
     instance_id = reservation['Instances'][0]['InstanceId']
 
-    tags = list(map(lambda x: create_tag_obj(x), tags_from_serverless_yaml.split(",")))
+    tags = list(map(lambda x: create_tag_obj(x), os.environ['AVAILABILITY_TAG_VALUES'].split(",")))
 
     client.create_tags(Resources=[instance_id], Tags=tags)
 
@@ -31,91 +44,89 @@ def test_should_stop_ec2_instances_with_matching_tags():
     assert instance_id in filtered_instance_ids
 
 
-@mock_ec2
-def test_should_not_stop_ec2_instances_without_matching_tag():
-    serverless_yaml = readYaml()
-    tags_from_serverless_yaml = serverless_yaml['provider']['environment']['TAGS']
-    os.environ['TAGS'] = tags_from_serverless_yaml
-    region = 'us-west-1'
-    client = boto3.client('ec2', region_name=region)
-    reservation = client.run_instances(ImageId='ami-1234abcd', MinCount=2, MaxCount=2)
-    instance_id = reservation['Instances'][0]['InstanceId']
-
-    tags = list(map(lambda x: create_tag_obj(x), tags_from_serverless_yaml.split(",")))
-
-    client.create_tags(Resources=[instance_id], Tags=tags)
-
-    handler.ec2_stop(None, None)
-
-    ec2 = boto3.resource('ec2', region_name=region)
-    instances = ec2.instances.filter(
-        Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
-    instance_id_list = list(map(lambda x: x.id, instances))
-    filtered_instance_ids = list(filter(lambda x: x == instance_id, instance_id_list))
-    assert instance_id in filtered_instance_ids
-
-    running_instances = ec2.instances.filter(
-        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-    running_instance_id_list = list(map(lambda x: x.id, running_instances))
-    assert len(running_instance_id_list) == 1
-
-
-@mock_ec2
-def test_should_match_multiple_tags():
-    os.environ['TAGS'] = 'Auto=On,Stop=true'
-    region = 'us-west-1'
-    client = boto3.client('ec2', region_name=region)
-    reservation = client.run_instances(ImageId='ami-1234abcd', MinCount=1, MaxCount=1)
-    instance_id = reservation['Instances'][0]['InstanceId']
-
-    tags = list(map(lambda x: create_tag_obj(x), os.environ['TAGS'].split(',')))
-
-    client.create_tags(Resources=[instance_id], Tags=tags)
-
-    handler.ec2_stop(None, None)
-
-    ec2 = boto3.resource('ec2', region_name=region)
-    instances = ec2.instances.filter(
-        Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
-    instance_id_list = list(map(lambda x: x.id, instances))
-    filtered_instance_ids = list(filter(lambda x: x == instance_id, instance_id_list))
-    assert instance_id in filtered_instance_ids
-
-
-@mock_ec2
-def test_stop_multiple_instances_with_different_tags():
-    os.environ['TAGS'] = 'Auto=On,Stop=true'
-    region = 'us-west-1'
-    client = boto3.client('ec2', region_name=region)
-    reservation = client.run_instances(ImageId='ami-1234abcd', MinCount=2, MaxCount=2)
-    instance_id = reservation['Instances'][0]['InstanceId']
-    instance_id_two = reservation['Instances'][1]['InstanceId']
-
-    tags = list(map(lambda x: create_tag_obj(x), 'Auto=On'.split(',')))
-
-    client.create_tags(Resources=[instance_id], Tags=tags)
-
-    tags = list(map(lambda x: create_tag_obj(x), 'Stop=true'.split(',')))
-
-    client.create_tags(Resources=[instance_id_two], Tags=tags)
-
-    handler.ec2_stop(None, None)
-
-    ec2 = boto3.resource('ec2', region_name=region)
-    instances = ec2.instances.filter(
-        Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
-    instance_id_list = list(map(lambda x: x.id, instances))
-    filtered_instance_ids = list(filter(lambda x: x == instance_id or x == instance_id_two, instance_id_list))
-    assert instance_id in filtered_instance_ids
-    assert instance_id_two in filtered_instance_ids
+#
+# @mock_ec2
+# def test_should_not_stop_ec2_instances_without_matching_tag():
+#     serverless_yaml = readYaml()
+#     tags_from_serverless_yaml = serverless_yaml['provider']['environment']['TAGS']
+#     os.environ['TAGS'] = tags_from_serverless_yaml
+#     region = 'ap-southeast-2'
+#     client = boto3.client('ec2', region_name=region)
+#     reservation = client.run_instances(ImageId='ami-1234abcd', MinCount=2, MaxCount=2)
+#     instance_id = reservation['Instances'][0]['InstanceId']
+#
+#     tags = list(map(lambda x: create_tag_obj(x), tags_from_serverless_yaml.split(",")))
+#
+#     client.create_tags(Resources=[instance_id], Tags=tags)
+#
+#     handler.ec2_stop(None, None)
+#
+#     ec2 = boto3.resource('ec2', region_name=region)
+#     instances = ec2.instances.filter(
+#         Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
+#     instance_id_list = list(map(lambda x: x.id, instances))
+#     filtered_instance_ids = list(filter(lambda x: x == instance_id, instance_id_list))
+#     assert instance_id in filtered_instance_ids
+#
+#     running_instances = ec2.instances.filter(
+#         Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+#     running_instance_id_list = list(map(lambda x: x.id, running_instances))
+#     assert len(running_instance_id_list) == 1
+#
+#
+# @mock_ec2
+# def test_should_match_multiple_tags():
+#     os.environ['TAGS'] = 'Auto=On,Stop=true'
+#     region = 'ap-southeast-2'
+#     client = boto3.client('ec2', region_name=region)
+#     reservation = client.run_instances(ImageId='ami-1234abcd', MinCount=1, MaxCount=1)
+#     instance_id = reservation['Instances'][0]['InstanceId']
+#
+#     tags = list(map(lambda x: create_tag_obj(x), os.environ['TAGS'].split(',')))
+#
+#     client.create_tags(Resources=[instance_id], Tags=tags)
+#
+#     handler.ec2_stop(None, None)
+#
+#     ec2 = boto3.resource('ec2', region_name=region)
+#     instances = ec2.instances.filter(
+#         Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
+#     instance_id_list = list(map(lambda x: x.id, instances))
+#     filtered_instance_ids = list(filter(lambda x: x == instance_id, instance_id_list))
+#     assert instance_id in filtered_instance_ids
+#
+#
+# @mock_ec2
+# def test_stop_multiple_instances_with_different_tags():
+#     os.environ['TAGS'] = 'Auto=On,Stop=true'
+#     region = 'ap-southeast-2'
+#     client = boto3.client('ec2', region_name=region)
+#     reservation = client.run_instances(ImageId='ami-1234abcd', MinCount=2, MaxCount=2)
+#     instance_id = reservation['Instances'][0]['InstanceId']
+#     instance_id_two = reservation['Instances'][1]['InstanceId']
+#
+#     tags = list(map(lambda x: create_tag_obj(x), 'Auto=On'.split(',')))
+#
+#     client.create_tags(Resources=[instance_id], Tags=tags)
+#
+#     tags = list(map(lambda x: create_tag_obj(x), 'Stop=true'.split(',')))
+#
+#     client.create_tags(Resources=[instance_id_two], Tags=tags)
+#
+#     handler.ec2_stop(None, None)
+#
+#     ec2 = boto3.resource('ec2', region_name=region)
+#     instances = ec2.instances.filter(
+#         Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
+#     instance_id_list = list(map(lambda x: x.id, instances))
+#     filtered_instance_ids = list(filter(lambda x: x == instance_id or x == instance_id_two, instance_id_list))
+#     assert instance_id in filtered_instance_ids
+#     assert instance_id_two in filtered_instance_ids
 
 
 def create_tag_obj(tag):
-    tag_split = tag.split("=")
-    tag_key = tag_split[0]
-    tag_value = tag_split[1]
-    return {'Key': tag_key,
-            'Value': tag_value}
+    return {'Key': 'Availability',
+            'Value': tag}
 
 
 def readYaml():
@@ -128,7 +139,7 @@ def readYaml():
 
 
 if __name__ == '__main__':
-    test_should_stop_ec2_instances_with_matching_tags()
-    test_should_not_stop_ec2_instances_without_matching_tag()
-    test_should_match_multiple_tags()
-    test_stop_multiple_instances_with_different_tags()
+    should_stop_ec2_instances_for_24x5_Mon_Fri_tag()
+    # test_should_not_stop_ec2_instances_without_matching_tag()
+    # test_should_match_multiple_tags()
+    # test_stop_multiple_instances_with_different_tags()
--- handler.py
+++ handler.py
@@ -1,32 +1,84 @@
 import os
+from datetime import datetime, timedelta
 
 import boto3
+import pytz
 
 
 def ec2_stop(event, context):
+    region = 'ap-southeast-2'
     print('Starting the ec2 stop lambda functionality')
     ec2_client = boto3.client('ec2')
     # Get list of regions
     regions = [region['RegionName']
                for region in ec2_client.describe_regions()['Regions']]
-    tags = os.environ['TAGS']
-    tags_filter_arr = list(map(lambda x: create_filter_obj(x), tags.split(",")))
-    # Iterate over each region
-    for region in regions:
-        ec2 = boto3.resource('ec2', region_name=region)
-        print("Region:", region)
-        # Get only running instances
-        all_found_instances = []
-        for created_tag_filter in tags_filter_arr:
-            instances = ec2.instances.filter(
-                Filters=[created_tag_filter, {'Name': 'instance-state-name', 'Values': ['running']}])
-            for instance in instances:
-                all_found_instances.append(instance)
-        # Stop the instances
-        for instance in all_found_instances:
-            instance.stop()
-            print('Stopped instance: ', instance.id)
-    print('Successfully executed the ec2 stop lambda functionality')
+    # tags = os.environ['TAGS']
+    tags = os.environ['AVAILABILITY_TAG_VALUES']
+    # tags_filter_arr = list(map(lambda x: create_filter_obj(x), tags.split(",")))
+    tags_filter_arr = get_eligible_filters(tags.split(","))
+    print(tags_filter_arr)
+    ec2 = boto3.resource('ec2', region_name=region)
+    # Get only running instances
+    all_found_instances = []
+    for created_tag_filter in tags_filter_arr:
+        instances = ec2.instances.filter(
+            Filters=[created_tag_filter, {'Name': 'instance-state-name', 'Values': ['running']}])
+        for instance in instances:
+            all_found_instances.append(instance)
+    # Stop the instances
+    for instance in all_found_instances:
+        instance.stop()
+        print('Stopped instance: ', instance.id)
+
+
+print('Successfully executed the ec2 stop lambda functionality')
+
+
+# def get_start_end_time_array():
+#     start_stop_arr = []
+#     now = datetime.now()
+#
+#     day_int = now.weekday()
+#     if day_int <= 5:
+#         start_date = now + timedelta(days=5 - day_int)
+#     else:
+#         start_date = now + timedelta(6)
+#     local_tz = pytz.timezone('Australia/Sydney')
+#     local_time = add.astimezone(local_tz)
+#     print(local_time)
+
+
+def get_eligible_filters(tags):
+    now = datetime.strptime(os.environ['CURR_TIME'],
+                            "%m/%d/%Y, %H:%M:%S") if "CURR_TIME" in os.environ is not None else datetime.now()
+    local_tz = pytz.timezone('Australia/Sydney')
+    local_time = now.astimezone(local_tz)
+    tags_arr = []
+    for tag in tags:
+        tag_start_end_date = get_valid_tags(tag)
+        if tag_start_end_date[0] <= local_time <= tag_start_end_date[1]:
+            print('found')
+            tags_arr.append({'Name': 'tag:{}'.format('Availability'),
+                             'Values': [tag]})
+    return tags_arr
+
+
+def get_valid_tags(pattern):
+    now = datetime.strptime(os.environ['CURR_TIME'],
+                            "%m/%d/%Y, %H:%M:%S") if "CURR_TIME" in os.environ is not None else datetime.now()
+    now = now.replace(hour=0, second=0, microsecond=0, minute=0)
+    local_tz = pytz.timezone('Australia/Sydney')
+    local_time = now.astimezone(local_tz)
+    day_int = now.weekday()
+    if pattern == "24x5_Mon-Fri":
+        print('paattern')
+        if day_int <= 5:
+            start_date = local_time + timedelta(days=5 - day_int)
+            end_date = local_time + timedelta(days=(5 - day_int) + 2)
+        else:
+            start_date = local_time + timedelta(days=day_int + 6)
+            end_date = local_time + timedelta(days=day_int + 1)
+        return start_date, end_date
 
 
 def create_filter_obj(tag):
@@ -38,4 +90,6 @@ def create_filter_obj(tag):
 
 
 if __name__ == '__main__':
-    ec2_stop(None, None)
+    # ec2_stop(None, None)
+    # print(get_valid_tags('24x5_Mon-Fri'))
+    get_eligible_filters(['24x5_Mon-Fri'])
