60d041051736a9c435b01e4fcffa2772ab32d663
dinukapryzm <dinuka.arseculeratne@pryzm.health>
2020-11-09 19:53:12 +1100

    fix test cases to run with UTC

--- handler.py
+++ handler.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime, timedelta,timezone
+from datetime import datetime, timedelta
 
 import boto3
 import pytz
@@ -9,12 +9,7 @@ def ec2_stop_start(event, context):
     region = 'ap-southeast-2'
     print('Starting the ec2 stop lambda functionality')
     ec2_client = boto3.client('ec2')
-    # Get list of regions
-    regions = [region['RegionName']
-               for region in ec2_client.describe_regions()['Regions']]
-    # tags = os.environ['TAGS']
     tags = os.environ['AVAILABILITY_TAG_VALUES']
-    # stop_tags_filter_arr = list(map(lambda x: create_filter_obj(x), tags.split(",")))
     stop_tags_filter_arr = get_eligible_stop_filters(tags.split(","))
     print('stop tags {}', stop_tags_filter_arr)
     ec2 = boto3.resource('ec2', region_name=region)
@@ -47,40 +42,27 @@ def ec2_stop_start(event, context):
 print('Successfully executed the ec2 stop lambda functionality')
 
 
-# def get_start_end_time_array():
-#     start_stop_arr = []
-#     now = datetime.now()
-#
-#     day_int = now.weekday()
-#     if day_int <= 5:
-#         start_date = now + timedelta(days=5 - day_int)
-#     else:
-#         start_date = now + timedelta(6)
-#     local_tz = pytz.timezone('Australia/Sydney')
-#     local_time = add.astimezone(local_tz)
-#     print(local_time)
-
-
 def get_eligible_stop_filters(tags):
     now = datetime.strptime(os.environ['CURR_TIME'],
                             "%m/%d/%Y, %H:%M:%S") if "CURR_TIME" in os.environ is not None else datetime.now()
     local_tz = pytz.timezone('Australia/Sydney')
     local_time = now.astimezone(local_tz)
     tags_arr = []
-    print(local_time)
     for tag in tags:
         tag_start_end_date = get_valid_tags(tag)
         if tag_start_end_date is not None and tag_start_end_date['stop_from_date'] is not None and \
                 tag_start_end_date['stop_to_date'] is not None and tag_start_end_date['stop_from_date'] <= local_time <= \
                 tag_start_end_date[
                     'stop_to_date']:
-            tags_arr.append({'Name': 'tag:{}'.format('Availability'),
+            # TODO: remove _test
+            tags_arr.append({'Name': 'tag:{}'.format('Availability_test'),
                              'Values': [tag]})
         else:
             if tag_start_end_date is not None and tag_start_end_date['stop_from_date'] is not None and \
                     tag_start_end_date[
                         'stop_from_date'] <= local_time:
-                tags_arr.append({'Name': 'tag:{}'.format('Availability'),
+                # TODO: remove _test
+                tags_arr.append({'Name': 'tag:{}'.format('Availability_test'),
                                  'Values': [tag]})
     return tags_arr
 
@@ -94,21 +76,20 @@ def get_eligible_start_filters(tags):
 
     for tag in tags:
         tag_start_end_date = get_valid_tags(tag)
-        print(tag_start_end_date)
         if tag_start_end_date is not None and tag_start_end_date['start_from_date'] is not None \
                 and tag_start_end_date['start_end_date'] is not None and local_time > \
                 tag_start_end_date['start_from_date'] and \
                 local_time < tag_start_end_date['start_end_date']:
-            print('found1')
-            tags_arr.append({'Name': 'tag:{}'.format('Availability'),
+            # TODO: remove _test
+            tags_arr.append({'Name': 'tag:{}'.format('Availability_test'),
                              'Values': [tag]})
         else:
             if tag_start_end_date is not None and tag_start_end_date['start_end_date'] is None \
                     and tag_start_end_date['start_from_date'] is not None \
                     and local_time > \
                     tag_start_end_date['start_from_date']:
-                print('found2')
-                tags_arr.append({'Name': 'tag:{}'.format('Availability'),
+                # TODO: remove _test
+                tags_arr.append({'Name': 'tag:{}'.format('Availability_test'),
                                  'Values': [tag]})
 
     return tags_arr
@@ -121,7 +102,6 @@ def get_valid_tags(pattern):
     local_tz = pytz.timezone('Australia/Sydney')
     local_time = now.astimezone(local_tz)
     day_int = now.weekday()
-    print('day int : {}'.format(day_int))
     if pattern == "24x5_Mon-Fri":
         if day_int > 0:
             start_from_date = local_time + timedelta(days=day_int - 4)
@@ -169,11 +149,3 @@ def get_valid_tags(pattern):
         start_end_date = None
     return {'stop_from_date': stop_from_date, 'stop_to_date': stop_to_date, 'start_from_date': start_from_date,
             'start_end_date': start_end_date}
-
-
-def create_filter_obj(tag):
-    tag_split = tag.split("=")
-    tag_key = tag_split[0]
-    tag_value = tag_split[1]
-    return {'Name': 'tag:{}'.format(tag_key),
-            'Values': [tag_value]}
